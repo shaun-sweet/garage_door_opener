b88efd7ab08d9b97e6ffe638ad8759d504696953
shaun-sweet <shaun@shaunsweet.com>
2016-11-15 03:22:40 +0000

    final

--- server.rb
+++ server.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
-set :bind, '10.0.1.16'
-get '/' do 
+set :bind, '10.0.1.55'
+get '/' do
 	puts "whats up"
 	erb :index
+end
+
+get '/open' do
+	system("python ~/projects/garage_door_opener/t.py")
+	redirect '/'
 end
