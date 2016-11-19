7758e7dab3bfc08c6d70bd92932cc66855978c40
sweetkeys <shaun@shaunsweet.com>
2016-11-18 18:28:19 -0700

    stndrd out update

--- server.rb
+++ server.rb
@@ -2,11 +2,11 @@ require 'sinatra'
 set :bind, '10.0.1.55'
 set :port, 80
 get '/' do
-	puts "whats up"
 	erb :index
 end
 
 get '/open' do
+  puts "Garage opening/closing"
 	system("python /home/pi/projects/garage_door_opener/t.py")
 	redirect '/'
 end
